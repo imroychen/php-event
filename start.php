03de37dc33f097e6939255fb2b4eda570f7770b9
roy <roy.chen@cifang.hk>
2021-07-27 15:14:53 +0800

    .

--- start.php
+++ start.php
@@ -2,8 +2,8 @@
 //手动加载
 spl_autoload_register(function ($class) {
     $classPath = str_replace('\\','/',rtrim($class,'\\'));
-    if(strpos($classPath,'ir/e')===0 && !class_exists($class,false)){
+    if(strpos($classPath,'iry/e')===0 && !class_exists($class,false)){
         //var_export(__DIR__.'/src/' . $classPath . '.php');echo "\n";
-        include str_replace('^ir/e/',__DIR__.'/src/', '^'.$classPath).'.php';
+        include str_replace('^iry/e/',__DIR__.'/src/', '^'.$classPath).'.php';
     }
 });
