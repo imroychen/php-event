4aa5547a797b63a18fe41a47acc470645ee9483f
roy <roy.chen@cifang.hk>
2021-08-26 13:26:10 +0800

    add print

--- example/client.php
+++ example/client.php
@@ -8,9 +8,9 @@ iry\e\App::setCfg('\\MyNamespace\\event\\Config');
 //客户端 开始测试触发事件
 $t = microtime(true);
 for ($i = 0;$i<100000;$i++){
-    //echo "Fire the \"Test\" \n";
+    echo "Fire the \"Test\" \n";
     //触发一个test事件.
     iry\e\Fire::event('test',['test'=>$i.'. '.date('H:i:s')],0);
-    //sleep(mt_rand(0,0));
+    sleep(mt_rand(0,3));
 }
 echo "\n".(microtime(true)-$t)."\n";
